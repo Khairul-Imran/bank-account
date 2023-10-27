b7bea5061c624d909fbf97c1f1e06836e8fb838e
Khairul <Khairulimran6810@gmail.com>
2023-10-27 14:30:16 +0800

    added to gitignore, and started work on main app

--- src/App.java
+++ src/App.java
@@ -1,29 +1,52 @@
+import java.io.Console;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to DBS!");
 
-        BankAccount bankAccountOne = new BankAccount();
-        BankAccount bankAccountTwo = new BankAccount("Khairul");
-        BankAccount bankAccountThree = new BankAccount("Bob",  (float) 1000);
-        
-        String accountNumberOne = bankAccountOne.getAccountNumber();
-        System.out.println("Account number: " + accountNumberOne);
+        // Insert the various commands that the user can give. (only at the beginning)
 
+        Console cons = System.console();
 
-        String accountNumberTwo = bankAccountTwo.getAccountNumber();
-        String accountNameTwo = bankAccountTwo.getName();
-        System.out.println("Account number: " + accountNumberTwo);
-        System.out.println("Account name: " + accountNameTwo);
+        boolean continueLoop = false;
+        String command = "";
+        String amount = "";
 
-        String accountNumberThree = bankAccountThree.getAccountNumber();
-        String accountNameThree = bankAccountThree.getName();
-        Float bankAccountBalanceThree = bankAccountThree.getBalance();
-        System.out.println("Account number: " + accountNumberThree);
-        System.out.println("Account name: " + accountNameThree);
-        System.out.println("Account balance: " + bankAccountBalanceThree);
+        while (!continueLoop) {
+            String input = cons.readLine("> ");
+            Scanner scanner = new Scanner(input.toLowerCase());
 
-        
+            switch (scanner.next()) {
+                case "open":
+                    // To show date the acount was opened, account number, account holder name
+                    break;
+                
+                case "close":
+                    // To show date the account was closed, account number, account holder name.
+                    break;
+
+                case "withdraw":
+                    // To show update balance, amount withdrew and at what time.
+                    break;
+
+                case "deposit":
+                    // To show update balance, amount deposited and at what time.
+                    break;
+
+                case "list transactions":
+                    // Lists transactions.
+                    break;
+
+                case "exit": 
+                    // Exits program.
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
 
 
     }
